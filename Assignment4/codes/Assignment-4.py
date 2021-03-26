8ce8a7d1c3f96997e7ed45c82bae5fa8dd984e2b
Amulya Tallamraju <80441552+AmulyaTallamraju@users.noreply.github.com>
2021-03-26 07:49:10 +0530

    Add files via upload

--- Assignment4/codes/Assignment-4.py
+++ Assignment4/codes/Assignment-4.py
@@ -1,6 +1,8 @@
  from scipy.stats import geom
  import numpy as np
  import matplotlib.pyplot as plt
+ import seaborn as sb
+ sb.set()
  sample=int(1000000)
  d=0
  n=0
@@ -12,6 +14,7 @@
    if (r[0]+r[1]==4) and (r[0]==2):
      n=n+1
  probab_sim=n/d
+ #calculating theoretical probability
  k=2
  x=4
  p_x=0
@@ -21,14 +24,49 @@
    p_x=p_x+(x1*x2)
  a=geom.pmf(2, p, loc=0)*geom.pmf(2, p, loc=0)/p_x # using independence of x1 x2
  probab_theo=a
- x = np.arange(1)
- plt.bar(x + 0.00, probab_theo, color = 'b', width = 0.25, label = 'Theoretical')
- plt.bar(x + 0.25, probab_sim, color = 'g', width = 0.25, label = 'Sim')
- #plt.xlabel('X=0')
- plt.ylabel('Probability')
- plt.xticks(x  + 0.25/2,['X1=2|X1+X2=4'])
- plt.legend()
- plt.show()
+
  print(f'The theoretical probability is {probab_theo} and the simulated probability is {probab_sim}')
- 
+b=[0,0,0,0,0,0,0,0,0,0]
+p_x=0
+
+i=2
+
+while i <12:
+  c=0
+  for k in range(1,i):
+    x1=geom.pmf(k, p, loc=0)
+    x2=geom.pmf(i-k, p, loc=0)
+    c=c+x1*x2
+    
+  
+  b[i-2]=c[0]
+  i=i+1
+  
+  
+
+
+#plotting
+fig,axes = plt.subplots(1,3, figsize=(15,5))
+fig.suptitle('PMF')
+
+x = np.arange(1,11)
+dist=geom(p)
+sb.barplot(ax=axes[0], x=x, y=dist.pmf(x))
+axes[0].set(xlabel='X1', ylabel='p(X1)')
+axes[0].set_title("PMF of X1")
+
+dist=geom(p)
+sb.barplot(ax=axes[1], x=x, y=dist.pmf(x))
+axes[1].set(xlabel='X2', ylabel='p(X2)')
+axes[1].set_title("PMF of X2")
+
+x=np.arange(2,12)
+dist=geom(p)
+sb.barplot(ax=axes[2], x=x, y=b)
+axes[2].set(xlabel='X', ylabel='p(X)')
+axes[2].set_title("PMF of X")
+
+# p=0.27047335 was the value used while plotting
+
+
  
